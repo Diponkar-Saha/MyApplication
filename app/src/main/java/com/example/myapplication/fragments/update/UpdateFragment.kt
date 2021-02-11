62d94f2e5826cbe7170168c9f9fb41b87f7fd8be
Diponkar Saha <44252172+Diponkar-Saha@users.noreply.github.com>
2021-02-11 22:28:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/fragments/update/UpdateFragment.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.fragments.update
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class UpdateFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_menu, menu);
+    }
+
+
+}
